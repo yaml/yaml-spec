7d663295a8bc3b931dfb2a447e8f629f5ff1c2ef
Ingy döt Net <ingy@ingy.net>
2021-09-05 10:39:02 -0700

    Fix regression with `make html` and add test for it

---
+++ test/test-make-html.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+source "$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)/setup"
+
+DIR=../www
+
+(
+  set -x
+
+  make -C "$DIR" force html FAST_TEST=1
+
+  [[ -e $DIR/html/spec.html ]]
+  [[ $(head -n1 $DIR/html/spec.html) == '<main'* ]]
+)
