e2eb28e12d2487de8425296f32dc2a7610351984
Ingy döt Net <ingy@ingy.net>
2021-09-06 06:09:28 -0700

    Fix shell lint warnings

--- test/test-make-html.sh
+++ test/test-make-html.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-source "$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)/setup"
+source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/setup"
 
 DIR=../www
 
 (
   set -x
 
-  make -C "$DIR" force html FAST_TEST=1
+  make -C $DIR force html FAST_TEST=1
 
   [[ -e $DIR/html/spec.html ]]
   [[ $(head -n1 $DIR/html/spec.html) == '<main'* ]]
--- test/test-www-build.sh
+++ test/test-www-build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-source "$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)/setup"
+source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/setup"
 
 DIR=../www
 
 (
   set -x
 
-  make -C "$DIR" force build FAST_TEST=1
+  make -C $DIR force build FAST_TEST=1
 
   [[ -e $DIR/spec.yaml.io/favicon.svg ]]
   [[ -e $DIR/spec.yaml.io/main/spec.html ]]
