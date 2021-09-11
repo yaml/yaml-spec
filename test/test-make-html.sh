0dbc7598df4b779f7338fe43d87599bb774a2cce
Ingy döt Net <ingy@ingy.net>
2021-09-11 14:56:03 -0700

    Generate html/*/title.html files

--- test/test-make-html.sh
+++ test/test-make-html.sh
@@ -10,12 +10,15 @@ DIR=../www
   make -C "$DIR" force html
 
   [[ -e $DIR/html/1.2.0/spec.html ]]
+  [[ -e $DIR/html/1.2.0/title.html ]]
   [[ -e $DIR/html/1.2.0/single_html.css ]]
 
   [[ -e $DIR/html/1.2.1/spec.html ]]
+  [[ -e $DIR/html/1.2.1/title.html ]]
   [[ -e $DIR/html/1.2.1/single_html.css ]]
 
   [[ -e $DIR/html/1.2.2/spec.html ]]
+  [[ -e $DIR/html/1.2.2/title.html ]]
   [[ -e $DIR/html/1.2.2/spec.css ]]
 
   [[ $(head -n1 "$DIR/html/1.2.2/spec.html") == '<main'* ]]
