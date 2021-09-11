2f29e6ed7cb2a7d328138fa16178853034a49a50
Ingy döt Net <ingy@ingy.net>
2021-09-11 16:41:49 -0700

    Add 1.2.2/img for make html

--- test/test-make-html.sh
+++ test/test-make-html.sh
@@ -12,14 +12,17 @@ DIR=../www
   [[ -e $DIR/html/1.2.0/spec.html ]]
   [[ -e $DIR/html/1.2.0/title.html ]]
   [[ -e $DIR/html/1.2.0/single_html.css ]]
+  [[ -e $DIR/html/1.2.0/model2.png ]]
 
   [[ -e $DIR/html/1.2.1/spec.html ]]
   [[ -e $DIR/html/1.2.1/title.html ]]
   [[ -e $DIR/html/1.2.1/single_html.css ]]
+  [[ -e $DIR/html/1.2.1/model2.png ]]
 
   [[ -e $DIR/html/1.2.2/spec.html ]]
   [[ -e $DIR/html/1.2.2/title.html ]]
   [[ -e $DIR/html/1.2.2/spec.css ]]
+  [[ -e $DIR/html/1.2.2/img/model2.svg ]]
 
   [[ $(head -n1 "$DIR/html/1.2.2/spec.html") == '<main'* ]]
 )
